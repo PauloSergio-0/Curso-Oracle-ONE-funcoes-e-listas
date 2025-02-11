701521482f39c901208921735c617730dfcc1772
Paulo Sérgio <paulosergiosrs08@gmail.com>
2025-02-10 23:23:16 -0300

    feat: init course

---
+++ src/app.js
@@ -0,0 +1,15 @@
+let titulo = document.querySelector('h1');
+let paragrafo = document.querySelector('p');
+
+titulo.innerHTML ='Jogo do número secreto';
+
+paragrafo.innerHTML = 'escolha um número entre 0 e 10.';
+
+function impimir(){
+    console.log('hahahahah');
+};
+
+
+function verificarChute(){
+    console.log('hahahahah1');
+};
